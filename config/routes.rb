39984abc317f540adc1bb1982c9d38e31002cf4a
Christopher Hayes <christopherhayes@gmail.com>
2013-08-06 21:32:38 -0400

    Added gravitar

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,8 @@ Treebook::Application.routes.draw do
   resources :statuses
   root :to => 'statuses#index'
 
+  get '/:id', to: 'profiles#show'
+
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
--- test/integration/custom_routes_test.rb
+++ test/integration/custom_routes_test.rb
@@ -17,4 +17,8 @@ end
 	assert_response :success
 end
 
+test "that a profile page works" do
+get '/chrishayes'
+assert_response :success
+
 end
